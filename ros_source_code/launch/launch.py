9a6c61ec683318c2dc9a6c4fdf49facb08a4ba94
once233 <62162310+once233@users.noreply.github.com>
2023-04-25 14:31:48 +0800

    Update launch.py

--- ros_source_code/launch/launch.py
+++ ros_source_code/launch/launch.py
@@ -1,5 +1,18 @@
 def generate_launch_description():        # 自动生成launch文件的函数
-    return LaunchDescription([            # 返回launch文件的描述信息
+    bringup_dir = get_package_share_directory('nav2_bringup') # 获取nav2_bringup文件夹下的share文件夹的路径
+    
+    container_name = LaunchConfiguration('container_name') # 用于在变量中存储启动参数的值并将它们传递给所需的变量（声明一个变量）
+    declare_params_file_cmd = DeclareLaunchArgument( #用于定义可以从上述启动文件或控制台传递的启动参数（给变量赋值）
+        'params_file', #定义的名称
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'), #默认值
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+    
+        configured_params = RewrittenYaml(
+        source_file=params_file,
+        root_key=namespace,
+        param_rewrites={},
+        convert_types=True)
+   return LaunchDescription([            # 返回launch文件的描述信息
 Node(
      condition= LaunchConfigurationEquals('map', ''),
      package='nav2_map_server'(工程名),
